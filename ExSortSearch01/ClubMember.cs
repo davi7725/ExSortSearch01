f0deb79e3a3fcdfc6c55f6718ed4cafb434180ca
davi7725 <davi7725@edu.eal.dk>
2017-03-07 11:34:38 +0100

    Add project files.

---
+++ ExSortSearch01/ClubMember.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExSortSearch01
+{
+    class ClubMember : IComparable
+    {
+        public int Nr { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public int Age { get; set; }
+
+        public ClubMember(int nr, string fname, string lname, int age)
+        {
+            Nr = nr;
+            Fname = fname;
+            Lname = lname;
+            Age = age;
+        }
+
+        public int CompareTo(object data)
+        {
+            int result = 0;
+            ClubMember cm = (ClubMember)data;
+
+            if(this.Nr > cm.Nr)
+            {
+                result = 1;
+            }
+            else if (this.Nr < cm.Nr)
+            {
+                result = -1;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool allEqual = false;
+            ClubMember cm = (ClubMember)obj;
+
+            if(this.Nr == cm.Nr && this.Fname == cm.Fname && this.Lname == cm.Lname && this.Age == cm.Age)
+            {
+                allEqual = true;
+            }
+
+            return allEqual;
+        }
+
+        public override int GetHashCode()
+        {
+            return Age.GetHashCode() + Fname.GetHashCode() + Lname.GetHashCode() + Nr.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Nr + " " + Fname + " " + Lname + " " + Age;
+        }
+    }
+}
---
+++ ExSortSearch01/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExSortSearch01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProg = new Program();
+            myProg.Run();
+        }
+
+        private void Run()
+        {
+            int smallSize = 1000;
+            int largeSize = smallSize * 10;
+
+            ClubMember[] smallLinear = new ClubMember[smallSize];
+            ClubMember[] largeLinear = new ClubMember[largeSize];
+
+            ClubMember[] smallBinary = new ClubMember[smallSize];
+            ClubMember[] largeBinary = new ClubMember[largeSize];
+
+            FillArray(smallLinear);
+            FillArray(largeLinear);
+            FillArray(smallBinary);
+            FillArray(largeBinary);
+
+            SearchLinear(smallLinear);
+            SearchLinear(largeLinear);
+
+            Console.ReadKey();
+        }
+
+        private void SearchLinear(IComparable[] arr)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Random rnd = new Random();
+
+            ClubMember[] cmArr = (ClubMember[])arr;
+
+            ClubMember cm1 = cmArr[rnd.Next(0, arr.Length - 1)];
+            ClubMember cm2 = cmArr[rnd.Next(0, arr.Length - 1)];
+            ClubMember cm3 = cmArr[rnd.Next(0, arr.Length - 1)];
+
+            int index = 0;
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                index = 0;
+                while (cm1.Equals(arr[index]) == false)
+                {
+                    index++;
+                }
+                index = 0;
+                while (cm2.Equals(arr[index]) == false)
+                {
+                    index++;
+                }
+                index = 0;
+                while (cm3.Equals(arr[index]) == false)
+                {
+                    index++;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine("Linear Search(" + arr.Length + "): " + sw.Elapsed);
+        }
+
+        private void FillArray(ClubMember[] arr)
+        {
+            for(int i =0; i<arr.Length;i++)
+            {
+                arr[i] = CMFactory.GetClubMember();
+            }
+        }
+    }
+}
